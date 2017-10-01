00771b17594bdfd1005befece1774c508aa4fcd2
Pedro Nazário <pnazario@globaldata.pt>
2017-10-01 16:22:18 +0100

    2nd version

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
+date_default_timezone_set('Europe/Lisbon');
 
 echo 'Olá!!';
+echo '<br/><br/>';
+$data = new DateTime();
+$nome = 'Pedro';
+
+var_dump($data);
+echo '<br/><br/>';
+/////////////////////////////////////////////////////
+
+$arquivo = fopen("index.php", "r");
+
+var_dump($arquivo);
+echo '<br/><br/>';
+/////////////////////////////////////////////////////
+
+function teste() {
+
+	echo 'Olá Boi!';
+}
+
+teste();
+echo '<br/><br/>';
+/////////////////////////////////////////////////////
+
+function teste2($nome) {
+
+	echo $nome;
+}
+
+teste2($nome);
 
 ?>
